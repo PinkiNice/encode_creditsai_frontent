3c3f3a7a58ff2b99ac5777c52e602a30b939178d
George Goshanov <goshaplutahin@gmail.com>
2024-10-27 01:39:18 +0100

    fix: update links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {useRouter} from 'next/navigation';
 import Footer from 'src/components/Footer';
 import TransactionWrapper from 'src/components/TransactionWrapper';
 import WalletWrapper from 'src/components/WalletWrapper';
@@ -7,6 +8,7 @@ import {useAccount} from 'wagmi';
 
 export default function Page() {
   const {address} = useAccount();
+  const router = useRouter();
 
   return (
     <div className="flex h-full w-96 max-w-full md:w-[1008px] flex-col px-1 ">
@@ -14,18 +16,17 @@ export default function Page() {
         <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
-              npm install @coinbase/onchainkit
+              encode_hack_creditsai
             </p>
           </div>
         </div>
-        {address ? (
-          <TransactionWrapper address={address} />
-        ) : (
-          <WalletWrapper
-            className="w-[450px] max-w-full"
-            text="Sign in to transact"
-          />
-        )}
+
+        <button
+          className="bg-blue-600 w-52 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl"
+          onClick={() => router.push('/marketplace')}
+        >
+          Okay, lets gooo {'->'}
+        </button>
       </section>
       <Footer />
     </div>
--- src/links.ts
+++ src/links.ts
@@ -1,6 +1,5 @@
 export const DISCORD_LINK = 'https://discord.gg/wTJ7besU';
-export const FIGMA_LINK =
-  'https://www.figma.com/community/file/1370194397345450683/onchainkit';
-export const GITHUB_LINK = 'https://github.com/coinbase/onchainkit';
-export const ONCHAINKIT_LINK = 'https://onchainkit.xyz';
-export const TWITTER_LINK = 'https://x.com/Onchainkit';
+
+export const GITHUB_LINK =
+  'https://github.com/PinkiNice/encode_creditsai_frontent';
+export const ONCHAINKIT_LINK = 'https://d902c1e78717.ngrok.app/';
